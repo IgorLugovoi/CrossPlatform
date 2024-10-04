ed799ffda202b0381e69f9fbfe7a3fc1430aa776
IgorLugovoi <qeadzc4065@knu.ua>
2024-10-05 00:19:24 +0300

    a

---
+++ Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static int[,] board;
+        static int N;
+
+        static void Main()
+        {
+            Task2 task = new Task2();
+            task.Start();
+        }        
+    }
+}
